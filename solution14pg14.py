72ecdd3fd8e2921a1f63d4bafed3a18d658a2482
jobizzz <jobisjames10@gmail.com>
2017-09-26 14:07:42 +0000

    14Task Submitted

---
+++ solution14pg14.py
@@ -0,0 +1,15 @@
+numOne = 2
+numTwo = 3 
+result = numOne & numTwo 
+print(result) 
+result = numOne | numTwo 
+print(result) 
+result = ~numTwo 
+print(result) 
+result = numTwo ^ numOne 
+print(result) 
+result = numOne << numTwo 
+print(result) 
+result = numOne >> numTwo 
+print(result) # Output -------- 2 3 -4 1 16
+
