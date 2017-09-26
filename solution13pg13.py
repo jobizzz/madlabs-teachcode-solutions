385755548b9925474797c0c5d3cde9fb0cf01814
jobizzz <jobisjames10@gmail.com>
2017-09-26 13:52:46 +0000

    13Task Submitted

---
+++ solution13pg13.py
@@ -0,0 +1,13 @@
+numOne = 0 
+numOne -= 0 #Modify this statement 
+numTwo = 50 
+numTwo += 2 #Modify this statement 
+numThree = 40 
+numThree -= 2 #Modify this statement 
+numFour = 20 
+numFour *= 2 #Modify this statement 
+numFive = 80 
+numFive /= 4 #Modify this statement 
+numSix = 100 
+numSix %= 10 #Modify this statement 
+print(numOne, numTwo, numThree, numFour, numFive, numSix)
