6d4e7ce9302839086ca1908aaa3cfed74efd7c58
jobizzz <jobisjames10@gmail.com>
2017-09-26 13:46:33 +0000

    12Task Submitted

---
+++ solution12pg12.py
@@ -0,0 +1,12 @@
+if 5 < 6:
+    print("Hi, welcome to this Game") 
+else:
+    print("You're not in the Game")
+for i in range(6):
+    if i == 3:
+        print("Google speaks Python")
+i=0
+while i < 10:
+    if i <= 1:
+        print("You have made it !")
+    i += 1
